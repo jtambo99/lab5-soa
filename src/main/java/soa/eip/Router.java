0cf4ca866c6f6600fd92731041daca777fd4764b
jtambo99 <jtambo@live.com>
2020-12-07 20:16:37 +0100

    Added 'max' parameter functionality

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -6,14 +6,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class Router extends RouteBuilder {
 
-  public static final String DIRECT_URI = "direct:twitter";
+    public static final String DIRECT_URI = "direct:twitter";
 
-  @Override
-  public void configure() {
-    from(DIRECT_URI)
-      .log("Body contains \"${body}\"")
-      .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
-      .log("Body now contains the response from twitter:\n${body}");
-  }
+    @Override
+    public void configure() {
+        from(DIRECT_URI)
+                .log("Body contains \"${body}\"")
+                .log("Searching twitter for \"${body}\"!")
+                // Source: https://camel.apache.org/components/latest/eips/process-eip.html
+                .process(exchange -> {
+                    String message = exchange.getIn().getBody(String.class);
+                    // keyboard could contain a 'max' command to limit the number of retrieved tweets
+                    if (message.matches("^.+ max:[0-9]+$")) {
+                        // Split the message by the delimiter ':'
+                        String[] splitted = message.split(":");
+                        // Obtain the last part:  .... max:<count> -> <count>
+                        int count = Integer.parseInt(splitted[splitted.length - 1]);
+                        // Set 'count' parameter with count
+                        exchange.getIn().setHeader("count", count);
+                        // Remove <count> from the message
+                        message = message.replace("max:" + count, "");
+                        exchange.getIn().setBody(message);
+                    } else {
+                        // By default, count = 5
+                        exchange.getIn().setHeader("count", 5);
+                    }
+                })
+                .log("count: ${header.count}")
+                .toD("twitter-search:${body}?count=${header.count}")
+                .log("Body now contains the response from twitter:\n${body}");
+    }
 }
